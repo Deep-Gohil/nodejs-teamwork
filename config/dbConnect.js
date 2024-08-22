11ca91fb36edf7ce69706c06c66272fa17a6b63d
Deep-Gohil <dep72617626@gmail.com>
2024-08-22 16:55:34 +0530

    second commit

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,1 +1,9 @@
-// Testing...
+const mongoose = require('mongoose');
+
+const databaseConnect = () =>{
+    mongoose.connect("mongodb+srv://deep888gohil:nodejs-teamwork@cluster0.1gr4v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",()=>{
+        console.log("Connected to MongoDB");
+    })
+}
+
+module.exports = databaseConnect;
