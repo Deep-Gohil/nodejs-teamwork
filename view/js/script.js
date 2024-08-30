f9b5e67f78cd0bcaabd8211b5671125fc29f9c9f
Deep-Gohil <deep72617626@gmail.com>
2024-08-30 16:55:03 +0530

    Updated

--- view/js/script.js
+++ view/js/script.js
@@ -1,9 +1,11 @@
 import navbar from "../components/navbar.js";
 document.getElementById("navbar").innerHTML = navbar()
 
+document.querySelector(".parentMain").style.display = "none";
 window.addEventListener('load', function() {
     setTimeout(function() {
       document.querySelector('.loader-parent').style.display = 'none';
+      document.querySelector(".parentMain").style.display = "block";
     }, 2000);
   });
   
