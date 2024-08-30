ebb33409edf8d3480496ee3dc5dc201ba2d9f45b
Deep-Gohil <deep72617626@gmail.com>
2024-08-30 13:56:32 +0530

    new

--- view/js/script.js
+++ view/js/script.js
@@ -1,4 +1,9 @@
 import navbar from "../components/navbar.js";
-
 document.getElementById("navbar").innerHTML = navbar()
-    
+
+window.addEventListener('load', function() {
+    setTimeout(function() {
+      document.querySelector('.loader-parent').style.display = 'none';
+    }, 2000);
+  });
+  
