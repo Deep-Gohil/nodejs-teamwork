d0c92309754e09b91c3ee475eaf7f78055d0fe72
Jaydip <146814713+Jaydip81@users.noreply.github.com>
2024-08-30 08:39:52 +0530

    Delete config directory

--- config/dbConnect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const databaseConnect = () =>{
-    mongoose.connect("mongodb+srv://deep888gohil:nodejs-teamwork@cluster0.1gr4v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",()=>{
-        console.log("Connected to MongoDB");
-    })
-}
-
-module.exports = databaseConnect;
