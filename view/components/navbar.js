868f54a7c56b7c34104412d65788f56fcd08dc28
Deep-Gohil <deep72617626@gmail.com>
2024-08-31 20:10:06 +0530

    Updated

--- view/components/navbar.js
+++ view/components/navbar.js
@@ -1,5 +1,31 @@
-const navbar = () =>{
-    return ``
+const navbar = () => {
+    return `<div class="beforeNav">
+        <div class="innerNavContainer">
+          <div class="welcome">
+            <p>Welcome To Our <span> Bame </span> Esports team | <span> <i class="fa-solid fa-earth-americas"></i> </span> Language</p>
+          </div>
+          <div class="links">
+            <div><p>Facebook</p> | <p>Twitter</p> | <p>Pintrest</p> | <p>Instagram</p></div>
+          </div>
+        </div>
+      </div>
+      <nav>
+        <div class="box1">
+          <img src="https://wordpress.themeholy.com/bame/wp-content/uploads/2023/12/logo.svg" alt="">
+        </div>
+        <div class="box2">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Tournaments</a>
+          <a href="#">Pages</a>
+          <a href="#">Blog</a>
+          <a href="#">Contact</a>
+        </div>
+        <div class="box3">
+          <i class="fa-solid fa-magnifying-glass"></i>
+          <i class="fa-solid fa-bars"></i>
+        </div>
+      </nav>`
 }
 
 export default navbar
--- view/js/script.js
+++ view/js/script.js
@@ -1,13 +1,12 @@
-// import navbar from "../components/navbar.js";
-//     document.getElementById("navbar").innerHTML = navbar();
+import navbar from "../components/navbar.js";
+document.getElementById("navbar").innerHTML = navbar();
 
-    // console.log("udehudh");
+// console.log("udehudh");
 
-    // document.querySelector(".parentMain").style.display = "none";
-    // window.addEventListener('load', function() {
-    //   setTimeout(function() {
-    //     document.querySelector('.loader-parent').style.display = 'none';
-    //     document.querySelector(".parentMain").style.display = "block";
-    //   }, 2000);
-    // });
-    
+document.querySelector(".parentMain").style.display = "none";
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    document.querySelector('.loader-parent').style.display = 'none';
+    document.querySelector(".parentMain").style.display = "block";
+  }, 2000);
+});
