2ce689419d4a6e1761eea60da177aab78e051a6b
Deep-Gohil <deep72617626@gmail.com>
2024-08-31 12:54:47 +0530

    Updated

--- view/js/script.js
+++ view/js/script.js
@@ -1,11 +1,12 @@
-import navbar from "../components/navbar.js";
-document.getElementById("navbar").innerHTML = navbar()
+// import navbar from "../components/navbar.js";
+//     document.getElementById("navbar").innerHTML = navbar();
 
-document.querySelector(".parentMain").style.display = "none";
-window.addEventListener('load', function() {
-    setTimeout(function() {
-      document.querySelector('.loader-parent').style.display = 'none';
-      document.querySelector(".parentMain").style.display = "block";
-    }, 2000);
-  });
-  
+    // console.log("udehudh");
+
+    // document.querySelector(".parentMain").style.display = "none";
+    // window.addEventListener('load', function() {
+    //   setTimeout(function() {
+    //     document.querySelector('.loader-parent').style.display = 'none';
+    //     document.querySelector(".parentMain").style.display = "block";
+    //   }, 2000);
+    // });
