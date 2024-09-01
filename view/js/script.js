fe0abb5410850769eae7042c77c59896aa3c2dc5
Deep-Gohil <deep72617626@gmail.com>
2024-09-01 17:07:10 +0530

    Updated

--- view/js/script.js
+++ view/js/script.js
@@ -10,3 +10,17 @@ window.addEventListener('load', function() {
     document.querySelector(".parentMain").style.display = "block";
   }, 2000);
 });
+
+
+const section = document.querySelector('.parentMain');
+const circle = document.querySelector('.circle');
+
+section.addEventListener('mousemove', (e) => {
+    const rect = section.getBoundingClientRect();
+    const x = e.clientX - rect.left; // X position within the section
+    const y = e.clientY - rect.top;  // Y position within the section
+    setTimeout(() => {
+    circle.style.left = `${x}px`;
+    circle.style.top = `${y}px`;
+  }, 150);
+});
