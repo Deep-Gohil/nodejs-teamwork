61afe72725e9ec31f9b8aa832215232be60f347f
Deep-Gohil <149695045+Deep-Gohil@users.noreply.github.com>
2024-09-03 08:29:07 +0530

    Update script.js

--- view/js/script.js
+++ view/js/script.js
@@ -3,13 +3,12 @@ document.getElementById("navbar").innerHTML = navbar();
 
 // console.log("udehudh");
 
-// document.querySelector(".parentMain").style.display = "none";
-// window.addEventListener('load', function() {
-//   setTimeout(function() {
-//     document.querySelector('.loader-parent').style.display = 'none';
-//     document.querySelector(".parentMain").style.display = "block";
-//   }, 2000);
-// });
+ document.querySelector(".parentMain").style.display = "none";
+window.addEventListener('load', function() {
+  setTimeout(function() {
+     document.querySelector('.loader-parent').style.display = 'none';
+     document.querySelector(".parentMain").style.display = "block";  }, 2000);
+ });
 
 
 const section = document.querySelector('.parentMain');
